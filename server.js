f64023a937bebd4b0def3f70e93845f86702ab6c
Govind <bardavalgovind2005@gmail.com>
2025-10-01 17:49:08 +0530

    updated

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.json());
 
 app.use("/calculate", deliveryRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is running. Use POST /calculate in Postman.");
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`)
